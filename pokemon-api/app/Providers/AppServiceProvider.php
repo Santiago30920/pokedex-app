679a10cfb3ce5cb9fdd84adf9df718fb4b5d4d1e
Santiago30920 <senirag@sougile.com>
2025-03-10 10:05:10 -0500

    Se añadio el dockerignore al proyecto

--- pokemon-api/app/Providers/AppServiceProvider.php
+++ pokemon-api/app/Providers/AppServiceProvider.php
@@ -22,11 +22,4 @@ class AppServiceProvider extends ServiceProvider
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
-    {
-        //
-    }
 }
